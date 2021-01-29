f4d350635250c7f11c765300f3ec30a87364455b
Justin Kang <68215015+justinkang01@users.noreply.github.com>
2021-01-29 15:01:42 -0500

    Small adjustments to Profile

--- scheduler-nextjs/components/profile.js
+++ scheduler-nextjs/components/profile.js
@@ -3,8 +3,13 @@ import Schedule from "../components/schedule"
 import utilStyles from '../styles/utils.module.css'
 import Tabs from '../components/tabs'
 
-const name = "Justin Kang"
 
+const user = {
+  userID: 123,
+  name: "Justin Kang",
+  email: "jk2375@cornell.edu",
+  schedules: null,
+}
 class Events extends Component {
 
 }
@@ -12,7 +17,8 @@ class Events extends Component {
 export default function Profile() {
   return (
     <div className={utilStyles.heading2X1}>
-      <h1>{name}</h1>
+      <h1>Name: {user.name}</h1>
+      <h1>Email: {user.email}</h1>
       <Tabs />
       <h4>test</h4>
     </div>
