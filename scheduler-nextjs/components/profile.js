c2cb934f67152fc968e0670d2cad6e349f883b76
Justin Kang <68215015+justinkang01@users.noreply.github.com>
2021-02-07 00:55:52 -0500

    Navbar
    Addition of primitive nav-bar to both pages along with the css module to style the navbar

--- scheduler-nextjs/components/profile.js
+++ scheduler-nextjs/components/profile.js
@@ -1,8 +1,9 @@
 import { Component } from "react";
 import Schedule from "../components/schedule"
 import utilStyles from '../styles/utils.module.css'
+import styles from "./profile.module.css"
 import Tabs from '../components/tabs'
-
+import Navbar from "./navbar"
 
 const user = {
   userID: 123,
@@ -16,11 +17,12 @@ class Events extends Component {
 
 export default function Profile() {
   return (
-    <div className={utilStyles.heading2X1}>
-      <h1>Name: {user.name}</h1>
-      <h1>Email: {user.email}</h1>
+    <>
+      <div className={styles.centered}>
+        <h1>Name: {user.name}</h1>
+        <h1>Email: {user.email}</h1>
+      </div>
       <Tabs />
-      <h4>test</h4>
-    </div>
+    </>
   )
 }
